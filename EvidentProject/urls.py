0bbe9f51f656328c3bb930c0969fb9268ace29dc
Mehedi Hasan Bijoy <mehedibubt34@gmail.com>
2021-09-22 14:30:06 +0600

    final

--- EvidentProject/urls.py
+++ EvidentProject/urls.py
@@ -20,8 +20,6 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,6 +28,3 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
